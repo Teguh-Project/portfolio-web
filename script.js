64390cd4b3904b277bbc716c32b067d3216c2554
Teguh P <teguhperwira27@gmail.com>
2026-04-26 22:59:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -183,3 +183,11 @@ window.addEventListener('scroll', () => {
 backToTop.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
